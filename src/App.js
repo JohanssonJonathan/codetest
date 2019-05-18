59972117ba29eb18a40c97343ce9b855c776625a
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2019-05-18 15:36:51 +0200

    the homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import Keypad from "./Keypad"
+import Design from "./Design"
 import "./App.css";
 
-const App = () => {
-  return <div>hej</div>;
-};
+
+
+class App extends Component {
+  state = {
+    keypad: false,
+    design: false
+  };
+
+  render() {
+    const { keypad, design } = this.state;
+    return (
+      <>
+        {(keypad || design) === false && (
+          <div className="choice-container flex-column">
+            <h2>Pick the challenge of your choice</h2>
+            <div className="flex-row">
+              <button
+                className="choice-button"
+                onClick={() => this.setState({ keypad: true })}
+              >
+                Keypad
+              </button>
+              <button
+                className="choice-button"
+                onClick={() => this.setState({ design: true })}
+              >
+                Design
+              </button>
+            </div>
+          </div>
+        )}
+
+        {keypad && <Keypad />}
+        {design && <Design />}
+      </>
+    );
+  }
+}
 
 export default App;
