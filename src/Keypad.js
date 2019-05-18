36af256afc817eb58138d8b6de08d3cff9a4638f
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2019-05-18 22:26:35 +0200

    refactor

--- src/Keypad.js
+++ src/Keypad.js
@@ -1,10 +1,87 @@
 import React, { Component } from "react";
 import "./App.css";
 import "./Keypad.css";
-import view from "./img/view.png"
-import hide from "./img/hide.png"
+import view from "./img/view.png";
+import hide from "./img/hide.png";
 
 import { CSSTransition } from "react-transition-group";
+
+const LoginOrRemoveButtonContainer = ({
+  inputfieldLength,
+  clickLogin,
+  clickRemove
+}) => {
+  return (
+    <div className="flex-flow button-container">
+      <button
+        style={{
+          backgroundColor: inputfieldLength > 0 ? "#429781" : "#76C2B7"
+        }}
+        onClick={() => clickLogin()}
+      >
+        Logga in
+      </button>
+      <button
+        style={{
+          backgroundColor: inputfieldLength === 0 ? "#F2B6B6" : "#F57E7E"
+        }}
+        onClick={clickRemove}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+const Numbers = ({ currentNumbers, focus, onMouseEnterLeave, onClick }) => {
+  return (
+    <ul>
+      {currentNumbers.map((item, i) => (
+        <li
+          className={item === null && "empty"}
+          id={focus === i && item !== null && "focus"}
+          onMouseEnter={() => {
+            onMouseEnterLeave(item, i);
+          }}
+          onMouseLeave={() => {
+            onMouseEnterLeave(item, i, "leave");
+          }}
+          onClick={() => onClick(item, i)}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const InputFieldContainer = ({
+  inputfield,
+  inputMessage,
+  showInput,
+  children
+}) => {
+  return (
+    <div className="input-container">
+      <CSSTransition
+        in={inputMessage}
+        timeout={200}
+        classNames="message"
+        onEntered={() => console.log("hej")}
+      >
+        <h5 className="message">Du kan endast trycka in en sex siffrig kod</h5>
+      </CSSTransition>
+      <input
+        placeholder="Ange din kod"
+        type={showInput ? "value" : "password"}
+        value={inputfield}
+        disabled={true}
+      />
+
+      {inputfield.length > 0 && children}
+    </div>
+  );
+};
 
 class Keypad extends Component {
   state = {
@@ -66,7 +143,13 @@ class Keypad extends Component {
       }));
     this.setTimeout("loggedIn", 2000);
   };
-
+  onMouseEnterLeave = (item, i, leave) => {
+    if (item !== null) {
+      this.setState({
+        focus: leave ? null : i
+      });
+    }
+  };
   removeNumber = () => {
     const { inputfield } = this.state;
     const newInputValue = inputfield.length - 1;
@@ -79,6 +162,10 @@ class Keypad extends Component {
   };
   keyPress = e => {
     const { inputfield, currentNumbers } = this.state;
+    if (e.key === "Enter") {
+      this.login();
+      return;
+    }
     currentNumbers.forEach((item, i) => {
       if (Number(e.key) === Number(item) && inputfield.length < 6) {
         this.setState(state => ({
@@ -89,9 +176,6 @@ class Keypad extends Component {
       this.setErrorMessage(inputfield.length);
     });
 
-    if (e.key === "Enter") {
-      this.login();
-    }
     if (e.key === "Backspace") {
       this.removeNumber();
     }
@@ -121,85 +205,48 @@ class Keypad extends Component {
           classNames="login-alert"
           onEntered={() => console.log("hej")}
         >
-            <div className="login-alert">
-              <h5>You logged in!</h5>
-              {/* <p>hej</p> */}
-            </div>
+          <div className="login-alert">
+            <h5>You logged in!</h5>
+          </div>
         </CSSTransition>
 
         <h4>Logga in med personlig kod</h4>
-
-        <div className="input-container">
-          <CSSTransition
-            in={inputMessage}
-            timeout={200}
-            classNames="message"
-            onEntered={() => console.log("hej")}
-          >
-            <h5 className="message">
-              Du kan endast trycka in en sex siffrig kod
-            </h5>
-          </CSSTransition>
-          <input
-            placeholder="Ange din kod"
-            type={showInput ? "value" : "password"}
-            value={inputfield}
-            disabled={true}
+        
+        <InputFieldContainer inputfield={inputfield} showInput={showInput} inputMessage={inputMessage}>
+          <img
+            src={showInput ? view : hide}
+            id="show"
+            onClick={() => this.setState({ showInput: !showInput })}
           />
+        </InputFieldContainer>
 
-          {inputfield.length > 0 && (
-            <img src={showInput?view:hide} id="show"
-              onClick={() => this.setState({ showInput: !showInput })}
-            />
-          )}
-        </div>
-        <ul>
-          {currentNumbers.map((item, i) => (
-            <li
-              className={item === null && "empty"}
-              id={focus === i && item !== null && "focus"}
-              onClick={() => {
-                if (item !== null && inputfield.length < 6) {
-                  this.setState({
-                    focus: i,
-                    inputfield: inputfield + item
-                  });
-                }
-                this.setErrorMessage(inputfield.length);
-              }}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-
-        <div className="flex-flow button-container">
-          <button
-            style={{
-              backgroundColor: inputfield.length > 0 ? "#429781" : "#76C2B7"
-            }}
-            onClick={() => {
-              this.login();
-            }}
-          >
-            Logga in
-          </button>
-          <button
-            style={{
-              backgroundColor: inputfield.length === 0 ? "#F2B6B6" : "#F57E7E"
-            }}
-            onClick={() => {
-              const newInputValue = inputfield.length - 1;
+        <Numbers
+          currentNumbers={currentNumbers}
+          focus={focus}
+          onMouseEnterLeave={this.onMouseEnterLeave}
+          onClick={(item, i) => {
+            if (item !== null && inputfield.length < 6) {
               this.setState({
-                inputfield: inputfield.substring(0, inputfield.length - 1),
-                focus: null,
-                inputMessage: newInputValue < 6 ? false : true
+                focus: i,
+                inputfield: inputfield + item
               });
-            }}
-          >
-            X
-          </button>
-        </div>
+            }
+            this.setErrorMessage(inputfield.length);
+          }}
+        />
+
+        <LoginOrRemoveButtonContainer
+          inputfieldLength={inputfield.length}
+          clickLogin={() => this.login()}
+          clickRemove={() => {
+            const newInputValue = inputfield.length - 1;
+            this.setState({
+              inputfield: inputfield.substring(0, inputfield.length - 1),
+              focus: null,
+              inputMessage: newInputValue < 6 ? false : true
+            });
+          }}
+        />
       </div>
     );
   }
